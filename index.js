88ba682b13e80a0e94400f66351efc81c39aff51
salmannoushad <salmansnoushad@gmail.com>
2021-05-28 16:15:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let count=0
+
+//select value and buttons
+const value =document.querySelector("#value")
+const buttons=document.querySelectorAll(".btn")
+
+
+buttons.forEach(function (btn){    //foreach method er maddhome sob gulike niye niyechi
+   btn.addEventListener('click', function(e){
+        const styles=e.currentTarget.classList
+        if(styles.contains("decrease")){
+            count--
+        }else if(styles.contains("increase")){
+            count++
+        }else{
+            count=0
+        }
+        if(count>0){
+            value.style.color="green"
+        }
+        if(count<0){
+            value.style.color="red"
+        }
+        if(count==0){
+            value.style.color="#222"
+        }
+
+        value.textContent=count
+   })
+})
